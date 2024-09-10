6469e0ce3c7515f8e8c5117460abda87a0890dc5
thiagoencd <thiagoencd@gmail.com>
2024-09-11 00:11:40 +0100

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -225,10 +225,10 @@ public class ChessMatch {
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 
-		for (char c = 'a'; c <= 'h'; c++) {
+		/*for (char c = 'a'; c <= 'h'; c++) {
 			placeNewPiece(c, 7, new Pawn(board, Color.BLACK));
 			placeNewPiece(c, 2, new Pawn(board, Color.WHITE));
-		}
+		}*/
 
 	}
 }
--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,62 @@ public class Bishop extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// NORTHWEST
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+			p.setColumn(p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// NORTHEAST
+
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+			p.setColumn(p.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// SOUTHWEST
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+			p.setColumn(p.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// SOUTHEAST
+		p.setValues(position.getRow() + 1, position.getColumn()+1);
+
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+			p.setColumn(p.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
 	}
 }
